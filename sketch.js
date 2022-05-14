16bd52373c0f3fb33434cbe616de16e6d311126b
AafreenKhan040896 <77886222+AafreenKhan040896@users.noreply.github.com>
2022-05-14 15:07:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+function preload() {
+  
+}
+
+function setup() {
+  canvas = createCanvas(1200,600);
+
+}
+
+function draw() {
+  background(189);
+  
+}
